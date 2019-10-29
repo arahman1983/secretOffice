f15e27554755e92d4a13d82f1ad463dd0c8d1860
a.rahman <a.rahman1783@yahoo.com>
2019-10-29 22:55:41 +0200

    all users page

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,16 @@
 let labelDataUrl = "/json/ar.json";
 
+
 $( document ).ready(()=>{
     // --------------------------- lables data ----------------------------------
+if($("html").attr("lang") === "en"){
+    labelDataUrl = "/json/en.json";
+    $("root").css("--direction", "ltr")
+}else{
+    labelDataUrl = "/json/ar.json";
+    $("root").css("--direction", "rtl")
+}
+
     fetch(labelDataUrl)
     .then(res=>res.json())
     .then(data=> {
@@ -11,18 +20,25 @@ $( document ).ready(()=>{
         $("#users").attr("title",data.users);
         $("#myFiles").attr("title",data.myFiles);
         $("#settings").attr("title",data.settings);
+        $("#logTitle").text(data.systemLog)
+        $("#usersTitle").text(data.users)
+        let tableHead = data.usersTable;
+
+        for(let i=0; i < tableHead.length; i++){
+            let j = i+1
+            $("#userTableHead th:nth-child("+j+")").text(tableHead[i].name)
+        }
+       
 
         let footerLinks = data.footerLinks;
         $("#footerLinks").html(
-            footerLinks.map(link=> " <li><a href='"+ link.link +"'>"+ link.name +"</a></li>")
+            footerLinks.map((link) => " <li><a href='"+ link.link +"'>"+ link.name +"</a></li>")
         )
        
     }
         )
 
 
-    //-------------------------- include in Index ------------------
-    $('#screens').load('logsDataTable.html');
 
 })
 
